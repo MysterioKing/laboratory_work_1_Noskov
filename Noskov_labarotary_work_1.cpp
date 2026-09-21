efd5333fc3c2d84083bb2308068566b70bf2ecb1
MysterioKing <noskovarseniy07@gmail.com>
2026-09-21 12:09:22 +0300

    Third commit. Update menu!

--- Noskov_labarotary_work_1.cpp
+++ Noskov_labarotary_work_1.cpp
@@ -1,38 +1,62 @@
 ﻿#include <iostream>
+#include <string>
 
 using namespace std;
+
+struct Pipe {
+	string name; // километровая отметка (название)
+	double l; // длина (в км)
+	int d; // диаметр (в мм)
+	bool flag; // признак "в ремонте"
+};
+
+struct CompressionStation {
+	string name; // название
+	int counts; // количество цехов
+	string classStation; // класс станции
+};
 
 
 int main() {
 	int flag;
-	cout << "Menu\n\nPrint number for continue\n\n1. Add pipe \n2. Add KC \n3. Check all objects \n4. Edit pipe \n5. Edit KC \n6. Save \n7. Upload \n0. Exit\n" << endl;
-	cin >> flag;
-	while (flag != 0) {
-		if (flag == 1) {
-			cout << "Add pipe:\n" << endl;
-		}
-		else if (flag == 2) {
-			cout << "Add KC:\n" << endl;
-		}
-		else if (flag == 3) {
-			cout << "Check all objects:\n" << endl;
-		}
-		else if (flag == 4) {
-			cout << "Edit pipe:\n" << endl;
-		}
-		else if (flag == 5) {
-			cout << "Edit KC:\n" << endl;
-		}
-		else if (flag == 6) {
-			cout << "Save\n" << endl;
-		}
-		else if (flag == 7) {
-			cout << "Upload\n" << endl;
-		}
-		else {
-			cout << "Error, this comand doesn't exist!\n" << endl;
-		}
+	
+	while (true) {
 		cout << "Menu\n\nPrint number for continue\n\n1. Add pipe \n2. Add KC \n3. Check all objects \n4. Edit pipe \n5. Edit KC \n6. Save \n7. Upload \n0. Exit\n" << endl;
 		cin >> flag;
+		switch (flag) {
+			case 0:
+				return 0;
+			case 1:
+				cout << "Add pipe:\n" << endl;
+				break;
+
+			case 2:
+				cout << "Add KC:\n" << endl;
+				break;
+
+			case 3:
+				cout << "Check all objects:\n" << endl;
+				break;
+
+			case 4:
+				cout << "Edit pipe:\n" << endl;
+				break;
+
+			case 5:
+				cout << "Edit KC:\n" << endl;
+				break;
+
+			case 6:
+				cout << "Save\n" << endl;
+				break;
+
+			case 7:
+				cout << "Upload\n" << endl;
+				break;
+
+			default:
+				cout << "Print number of exist function!" << endl;
+				break;
+	}
 	}
 }
